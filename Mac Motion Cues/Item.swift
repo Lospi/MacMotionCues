4705e069a6766109055ebdef90cdcb454f6f5bf0
Roberto <roberto3154_421@hotmail.com>
2024-11-21 21:52:24 -0300

    Initial Commit

---
+++ Mac Motion Cues/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Mac Motion Cues
+//
+//  Created by Roberto Camargo on 21/11/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
